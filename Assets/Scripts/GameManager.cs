9389ad00a2d5ca35628554b183d5b2bdeca21a5a
QuadeSr <106781080+QuadeSr@users.noreply.github.com>
2023-03-10 14:37:21 -0500

    prototype title screen added

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        // Make sure this is a singleton and that it survives between scene changes
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Starts a new game
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
